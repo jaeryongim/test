fc4dda2115b3c87b7c1b49ce9956919e090eba83
steveim <steveim1201@gmail.com>
2016-03-12 03:20:11 +0000

    create timeline wrting delete page

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -20,14 +20,18 @@ class WallController < ApplicationController
   
   def edit_complete
     p = Post.find(params[:id])
-    p.name = params[:write_edit]
+    p.name = params[:writer_edit]
     p.content = params[:content_edit]
     if p.save
-      redirect_to "/wall.posts"
+      redirect_to "/wall/posts"
     else
       flash[:alert] = p.errors[:content][0]
       redirect_to :back
     end
+  end
+  
+  def delete
+    @posts_delete = Post.find(params[:id])
   end
   
   def posts
