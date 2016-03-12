20de54d97810989555b61539eaed855ac85ae5f8
steveim <steveim1201@gmail.com>
2016-03-12 03:04:20 +0000

    adding save & change after wrting modify

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -18,7 +18,21 @@ class WallController < ApplicationController
     @posts_edit = Post.find(params[:id])
   end
   
+  def edit_complete
+    p = Post.find(params[:id])
+    p.name = params[:write_edit]
+    p.content = params[:content_edit]
+    if p.save
+      redirect_to "/wall.posts"
+    else
+      flash[:alert] = p.errors[:content][0]
+      redirect_to :back
+    end
+  end
+  
   def posts
     @posts = Post.all
   end
+  
+  
 end
