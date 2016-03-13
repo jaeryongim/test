203ded6b56c99285efc5a8fb6f7305c753e5aecf
steveim <steveim1201@gmail.com>
2016-03-13 04:47:56 +0000

    adding login status check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+  
+  def login_check
+    @current_user = nil
+    if !cookies[:user_id].nil?
+      @cureent_user = User.find(cookies[:user_id])
+    else
+      redirect_to "/user/login"
+    end
+  end
+  
+  
 end
